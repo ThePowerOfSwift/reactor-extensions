b2d1d5b0f67470d59084e828a110384f37e849d6
Matt McArthur <matt@cotopaxi.com>
2017-09-14 15:03:38 -0600

    Initial Commit

---
+++ ReactorExtensions/ReactorExtensions.h
@@ -0,0 +1,19 @@
+//
+//  ReactorExtensions.h
+//  ReactorExtensions
+//
+//  Created by Matthew McArthur on 9/14/17.
+//  Copyright © 2017 McArthur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactorExtensions.
+FOUNDATION_EXPORT double ReactorExtensionsVersionNumber;
+
+//! Project version string for ReactorExtensions.
+FOUNDATION_EXPORT const unsigned char ReactorExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactorExtensions/PublicHeader.h>
+
+
